c562ab0063f583e4c35d29059f2f4deeb485e5c9
Celine Worm <wormceline@gmail.com>
2019-10-07 14:05:32 +0200

    commit all

---
+++ todo/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <ToDoList />
+  </div>
+</template>
+
+<script>
+import ToDoList from "@/components/ToDoList.vue";
+
+export default {
+  name: "app",
+  components: {
+    ToDoList
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
